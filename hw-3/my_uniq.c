e34209941f6f89b87bc0a59978e9d13a90ca0721
Sean Yap <syap0317@gmail.com>
2021-03-05 23:30:16 -0500

    add comment structure of code

---
+++ hw-3/my_uniq.c
@@ -0,0 +1,12 @@
+// open file with system call
+  // handle error
+
+// create buffer
+
+// read in one line 
+// loop through rest of the lines and check with previous line
+    // if both lines are the same
+        // skip to next iteration
+    // add line to buffer
+
+// print buffer

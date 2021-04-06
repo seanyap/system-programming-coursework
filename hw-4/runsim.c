a604ad7a4c094029cd881d42142357da5e5c3cba
Sean Yap <syap0317@gmail.com>
2021-04-06 01:21:17 -0400

    add fork feature and mchange the scope of some variables

--- hw-4/runsim.c
+++ hw-4/runsim.c
@@ -1,6 +1,7 @@
 #include <stdio.h> // for printf(), fgets()
 #include <stdlib.h> // for type EXIT_FAILURE and atoi()
-#include <unistd.h> // for STDIN_FILENO, STDOUT_FILENO, STDERR_FILENO
+#include <sys/types.h>
+#include <unistd.h> // for getpid(), getppid()
 
 // define constants
 const int MAX_CHILDREN = 1000;
@@ -17,6 +18,9 @@ int main(int argc, char *argv[])
   int max_proc;
   int proc_c = 0;
   char *buffer = (char *) malloc(MAX_SIZE * sizeof(char));
+  char *ptr = buffer;
+  char *filepath = buffer;
+  char *p_arr[MAX_SIZE];
 
   // convert string arg from cmd line to int 
   int n_of_proc = atoi(argv[1]);  // assume user input expected value: int 
@@ -30,36 +34,49 @@ int main(int argc, char *argv[])
       if (proc_c == max_proc) {
         // TODO wait on child process
         // proc_c -= 1;
+        printf("SUCCESS!!\n");
+        exit(EXIT_SUCCESS);
       }
       // read input line from stdin to buffer
       if (fgets(buffer, MAX_SIZE-1, stdin)) {
-        char *ptr = buffer;
-        char *path = buffer;
-        char * p_arr[MAX_SIZE];
-        
         int count = 0;
-        // set pointers that point at each arguments 
+        // set pointers in arr to point at each arguments provided
         while(*ptr != '\n') {
           // found one argument
           if (*ptr == ' ' || *ptr == '\t') {
             *ptr = '\0';// replace cur char with NULL char
             p_arr[count] = ++ptr;
             count += 1;
-          } else {
+          } 
+          else {
             ptr += 1;
           }
         }
         p_arr[count] = NULL; // set last pointer in pointers array NULL
 
-        for(char **p = p_arr; *p; p++)
-          printf("pointer = %s\n", *p);
+        // fork and exec
+        pid_t pid = fork(); 
+        if (pid == -1) {                 // error forking
+          perror("Can not fork");
+          exit(EXIT_FAILURE);
+        }
+        else if (pid == 0) {             // inside child
+          printf("child successfully forked! pid: %d\n", getpid());
+          // TODO execv
+          exit(EXIT_SUCCESS);
+        }
+        else {                           // inside parent
+          printf("parent pid: %d\n", getpid());          
+        }
 
-        printf("Line loaded into path = %s\n",path);
+        // printf("filepath = %s\n",filepath);
+        // for(char **p = p_arr; *p; p++)
+        //   printf("pointer = %s\n", *p);
       } 
-      // else {
-      //   printf("Error reading from standard input");
-      //   exit(EXIT_FAILURE);
-      // }
+      else {
+        printf("Error reading from standard input\n");
+        exit(EXIT_FAILURE);
+      }
 
       proc_c += 1;
     }
@@ -68,6 +85,8 @@ int main(int argc, char *argv[])
     printf("Error! Invalid number of running processes\n");
     exit(EXIT_FAILURE);
   }
+
+  free(buffer); //free array on heap
 
   return EXIT_SUCCESS;
 }

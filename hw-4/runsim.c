1ad6c5eeb764dc6918331d09f0c24f85a03330f7
Sean Yap <syap0317@gmail.com>
2021-04-06 16:40:22 -0400

    add functionality to create and wait for child processes

--- hw-4/runsim.c
+++ hw-4/runsim.c
@@ -4,7 +4,7 @@
 #include <unistd.h> // for getpid(), getppid()
 
 // define constants
-const int MAX_CHILDREN = 1000;
+const int MAX_PROC = 1000;
 const int MAX_SIZE = 1000;
 
 int main(int argc, char *argv[]) 
@@ -15,75 +15,80 @@ int main(int argc, char *argv[])
     exit(EXIT_FAILURE);
   }
   
-  int max_proc;
-  int proc_c = 0;
+  // int max_proc;
+  int proc_c = 0;   // process counter
   char *buffer = (char *) malloc(MAX_SIZE * sizeof(char));
   char *ptr = buffer;
   char *filepath = buffer;
   char *p_arr[MAX_SIZE];
 
-  // convert string arg from cmd line to int 
+  // convert char arg to int 
   int n_of_proc = atoi(argv[1]);  // assume user input expected value: int 
   // TODO check for error; wrong type
 
   // check the number of processes user specified is within allowable bounds
-  if (n_of_proc >= 1 && n_of_proc <= MAX_CHILDREN) {
-    max_proc = n_of_proc;
+  if (n_of_proc < 1 || n_of_proc > MAX_PROC) {
+    printf("Error! Invalid number of running processes\n");
+    exit(EXIT_FAILURE);
+  }
 
-    while (proc_c <= max_proc) {  // TODO add EOF check
-      if (proc_c == max_proc) {
-        // TODO wait on child process
-        // proc_c -= 1;
-        printf("SUCCESS!!\n");
-        exit(EXIT_SUCCESS);
-      }
-      // read input line from stdin to buffer
-      if (fgets(buffer, MAX_SIZE-1, stdin)) {
-        int count = 0;
-        // set pointers in arr to point at each arguments provided
-        while(*ptr != '\n') {
-          // found one argument
-          if (*ptr == ' ' || *ptr == '\t') {
-            *ptr = '\0';// replace cur char with NULL char
-            p_arr[count] = ++ptr;
-            count += 1;
-          } 
-          else {
-            ptr += 1;
-          }
-        }
-        p_arr[count] = NULL; // set last pointer in pointers array NULL
+  while (fgets(buffer, MAX_SIZE-1, stdin)) {    // EOF check
+    if (proc_c == n_of_proc) {
+      wait(NULL);     // wait on child process
+      printf("waited\n");
+      proc_c -= 1;
+    }
 
-        // fork and exec
-        pid_t pid = fork(); 
-        if (pid == -1) {                 // error forking
-          perror("Can not fork");
-          exit(EXIT_FAILURE);
-        }
-        else if (pid == 0) {             // inside child
-          printf("child successfully forked! pid: %d\n", getpid());
-          // TODO execv
-          exit(EXIT_SUCCESS);
-        }
-        else {                           // inside parent
-          printf("parent pid: %d\n", getpid());          
-        }
-
-        // printf("filepath = %s\n",filepath);
-        // for(char **p = p_arr; *p; p++)
-        //   printf("pointer = %s\n", *p);
+    // process buffer and create pointers to each arg in buffer
+    int count = 1;
+    // set pointers in arr to point at each arguments provided
+    while(*ptr != '\n') {
+      // found one argument
+      if (*ptr == ' ' || *ptr == '\t') {
+        *ptr = '\0';// replace cur char with terminating char
+        p_arr[count] = ++ptr;
+        count += 1;
       } 
       else {
-        printf("Error reading from standard input\n");
-        exit(EXIT_FAILURE);
+        ptr += 1;
       }
+    }
+    p_arr[0] = filepath;
+    p_arr[count] = NULL; // set last pointer in pointers array to NULL pointer
 
+    // fork and exec
+    pid_t pid = fork(); 
+    if (pid == -1) {                 // error forking
+      perror("Can not fork");
+      exit(EXIT_FAILURE);
+    }
+    else if (pid == 0) {             // inside child
+      printf("Child process with pid %d successfully forked!\n", getpid());
+      execv(filepath, p_arr);
       proc_c += 1;
     }
 
-  } else {
-    printf("Error! Invalid number of running processes\n");
-    exit(EXIT_FAILURE);
+    // check if any child has terminated
+    int stat;
+    pid_t cpid = waitpid(0, &stat, WNOHANG);  
+    if (cpid != 0) {
+      printf("Child process with pid %d: Normal termination with exit status = %d\n", cpid, WEXITSTATUS(stat));
+      proc_c -= 1;
+    } 
+
+    // Test to check if correct args were read into buffer
+    // printf("filepath = %s\n",filepath);
+    // for(char **p = p_arr; *p; p++)
+    //   printf("pointer = %s\n", *p); 
+  }
+
+  // TODO wait on remaining children that are still running
+  int pid, stat;
+  while (proc_c > 0) {
+    if ((pid = waitpid(0, &stat, WNOHANG)) != 0) {
+      printf("Child process with pid %d: Normal termination with exit status = %d\n", pid, WEXITSTATUS(stat));
+      proc_c -= 1;
+    }
   }
 
   free(buffer); //free array on heap

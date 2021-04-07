814959efe727a8633c7514c2ddd2a0016d155922
Sean Yap <syap0317@gmail.com>
2021-04-06 23:28:00 -0400

    change error message

--- hw-4/runsim.c
+++ hw-4/runsim.c
@@ -27,7 +27,7 @@ int main(int argc, char *argv[])
   // convert char arg to int 
   int n_of_proc = atoi(argv[1]);  
   if (n_of_proc == 0) {
-      perror("Invalid conversion");
+      perror("Invalid argument!");
       exit(EXIT_FAILURE);
   }
 
